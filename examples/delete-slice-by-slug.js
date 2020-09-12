a086346706ceab4490762073c1a7020b819d70fc
Eric Carraway <eric@ericcarraway.com>
2020-09-12 16:19:44 +0200

    add usage example for `deleteSliceBySlug`

---
+++ examples/delete-slice-by-slug.js
@@ -0,0 +1,25 @@
+const { apiClient } = require(`./index.js`);
+
+// change this to the slug of a slice you own & wish to delete
+const slug = `456420`;
+
+const main = async () => {
+  try {
+    const res = await apiClient.deleteSliceBySlug(slug);
+
+    // { name: 'Fibonacci Konnakol', artist: 'B C Manjunath' }
+    console.log(res.data);
+  } catch (err) {
+    console.error(`ERROR:`);
+
+    const { status, statusText } = err.response;
+
+    // { status: 403, statusText: 'Forbidden' }
+    console.log({
+      status,
+      statusText,
+    });
+  }
+};
+
+main();
