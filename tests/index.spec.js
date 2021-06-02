4c24ebb06f54a00776e82658124114efbcbca8eb
Eric Carraway <eric@ericcarraway.com>
2021-06-02 20:38:50 +0200

    convert smoke tests in `tests/index.spec.js` from `mocha` to `jest`

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,10 +1,12 @@
-require(`dotenv`).config();
+/* global describe, it, expect */
 
-const { expect } = require(`chai`);
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require(`dotenv`).config();
 
 const { SOUNDSLICE_APPLICATION_ID, SOUNDSLICE_PASSWORD } = process.env;
 
-const apiClient = require(`../index.js`)({
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const apiClient = require(`../dist/index`)({
   SOUNDSLICE_APPLICATION_ID,
   SOUNDSLICE_PASSWORD,
 });
@@ -38,134 +40,136 @@ describe(`soundslice-data-api`, () => {
         `uploadFile`,
       ];
 
-      expect(actual).to.eql(expected);
+      expect(actual).toEqual(expected);
     });
   });
 
   describe(`createFolder`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.createFolder).to.be.a(`function`);
+      expect(typeof apiClient.createFolder).toBe(`function`);
     });
   });
 
   describe(`createRecording`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.createRecording).to.be.a(`function`);
+      expect(typeof apiClient.createRecording).toBe(`function`);
     });
   });
 
   describe(`createSlice`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.createSlice).to.be.a(`function`);
+      expect(typeof apiClient.createSlice).toBe(`function`);
     });
   });
 
   describe(`deleteFolderByFolderId`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.deleteFolderByFolderId).to.be.a(`function`);
+      expect(typeof apiClient.deleteFolderByFolderId).toBe(`function`);
     });
   });
 
   describe(`deleteRecordingByRecordingId`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.deleteRecordingByRecordingId).to.be.a(`function`);
+      expect(typeof apiClient.deleteRecordingByRecordingId).toBe(`function`);
     });
   });
 
   describe(`deleteSliceBySlug`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.deleteSliceBySlug).to.be.a(`function`);
+      expect(typeof apiClient.deleteSliceBySlug).toBe(`function`);
     });
   });
 
   describe(`duplicateSliceByScorehash`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.duplicateSliceByScorehash).to.be.a(`function`);
+      expect(typeof apiClient.duplicateSliceByScorehash).toBe(`function`);
     });
   });
 
   describe(`getRecordingUploadUrlByRecordingId`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.getRecordingUploadUrlByRecordingId).to.be.a(`function`);
+      expect(typeof apiClient.getRecordingUploadUrlByRecordingId).toBe(
+        `function`,
+      );
     });
   });
 
   describe(`getSliceByScorehash`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.getSliceByScorehash).to.be.a(`function`);
+      expect(typeof apiClient.getSliceByScorehash).toBe(`function`);
     });
   });
 
   describe(`getSliceBySlug`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.getSliceBySlug).to.be.a(`function`);
+      expect(typeof apiClient.getSliceBySlug).toBe(`function`);
     });
   });
 
   describe(`getSliceNotationBySlug`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.getSliceNotationBySlug).to.be.a(`function`);
+      expect(typeof apiClient.getSliceNotationBySlug).toBe(`function`);
     });
   });
 
   describe(`getSliceRecordingsByScorehash`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.getSliceRecordingsByScorehash).to.be.a(`function`);
+      expect(typeof apiClient.getSliceRecordingsByScorehash).toBe(`function`);
     });
   });
 
   // @deprecated since 2021-01-13
   describe(`getSliceRecordingsBySlug`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.getSliceRecordingsBySlug).to.be.a(`function`);
+      expect(typeof apiClient.getSliceRecordingsBySlug).toBe(`function`);
     });
   });
 
   describe(`getSyncpointsByRecordingId`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.getSyncpointsByRecordingId).to.be.a(`function`);
+      expect(typeof apiClient.getSyncpointsByRecordingId).toBe(`function`);
     });
   });
 
   describe(`listFolders`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.listFolders).to.be.a(`function`);
+      expect(typeof apiClient.listFolders).toBe(`function`);
     });
   });
 
   describe(`listSlices`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.listSlices).to.be.a(`function`);
+      expect(typeof apiClient.listSlices).toBe(`function`);
     });
   });
 
   describe(`listSubfoldersByParentId`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.listSubfoldersByParentId).to.be.a(`function`);
+      expect(typeof apiClient.listSubfoldersByParentId).toBe(`function`);
     });
   });
 
   describe(`moveSliceToFolder`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.moveSliceToFolder).to.be.a(`function`);
+      expect(typeof apiClient.moveSliceToFolder).toBe(`function`);
     });
   });
 
   describe(`putRecordingSyncpoints`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.putRecordingSyncpoints).to.be.a(`function`);
+      expect(typeof apiClient.putRecordingSyncpoints).toBe(`function`);
     });
   });
 
   describe(`renameFolder`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.renameFolder).to.be.a(`function`);
+      expect(typeof apiClient.renameFolder).toBe(`function`);
     });
   });
 
   describe(`uploadFile`, () => {
     it(`should be a function`, () => {
-      expect(apiClient.uploadFile).to.be.a(`function`);
+      expect(typeof apiClient.uploadFile).toBe(`function`);
     });
   });
 });
