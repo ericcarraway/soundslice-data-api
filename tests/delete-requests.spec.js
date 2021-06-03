ebc581961f589865c33d4d517c88fb0d29c295a2
Eric Carraway <eric@ericcarraway.com>
2021-06-03 23:52:25 +0200

    add Jest tests for all of the methods that make HTTP DELETE requests

---
+++ tests/delete-requests.spec.js
@@ -0,0 +1,84 @@
+/* global describe, expect, test, jest */
+
+const { getApiClientInstance } = require(`../dist/get-api-client-instance.js`);
+
+const {
+  EXPECTED_BASE_AXIOS_CONFIG,
+  SOUNDSLICE_APPLICATION_ID,
+  SOUNDSLICE_PASSWORD,
+} = require(`./test-helpers.js`);
+
+const makeMockAxios = () => ({ delete: jest.fn() });
+
+// // all DELETE methods...
+// const deleteFolderByFolderId = (folderId: number | string) =>
+//   axiosWrapper.delete(`/folders/${folderId}/`);
+
+// const deleteRecordingByRecordingId = (recordingId: number | string) =>
+//   axiosWrapper.delete(`/recordings/${recordingId}/`);
+
+// // TODO: add `deleteSliceByScorehash` and mark this as 'no longer documented'
+// const deleteSliceBySlug = (slug: number | string) =>
+//   axiosWrapper.delete(`/scores/${slug}/`);
+
+describe(`DELETE requests`, () => {
+  test(`.deleteFolderByFolderId method`, async () => {
+    const mockAxios = makeMockAxios();
+    const apiClient = getApiClientInstance({
+      axios: mockAxios,
+      SOUNDSLICE_APPLICATION_ID,
+      SOUNDSLICE_PASSWORD,
+    });
+
+    const folderId = `4321`;
+    const unusedResult = await apiClient.deleteFolderByFolderId(folderId);
+
+    expect(mockAxios.delete).toHaveBeenCalled();
+    expect(mockAxios.delete).toHaveBeenCalledWith(
+      `/folders/4321/`,
+      EXPECTED_BASE_AXIOS_CONFIG,
+    );
+  });
+
+  test(`.deleteRecordingByRecordingId method`, async () => {
+    const mockAxios = makeMockAxios();
+    const apiClient = getApiClientInstance({
+      axios: mockAxios,
+      SOUNDSLICE_APPLICATION_ID,
+      SOUNDSLICE_PASSWORD,
+    });
+
+    const recordingId = `2468`;
+    const unusedResult = await apiClient.deleteRecordingByRecordingId(
+      recordingId,
+    );
+
+    expect(mockAxios.delete).toHaveBeenCalled();
+    expect(mockAxios.delete).toHaveBeenCalledWith(
+      `/recordings/2468/`,
+      EXPECTED_BASE_AXIOS_CONFIG,
+    );
+  });
+
+  test.skip(`.deleteSliceByScorehash method`, async () => {
+    //
+  });
+
+  test(`.deleteSliceBySlug method`, async () => {
+    const mockAxios = makeMockAxios();
+    const apiClient = getApiClientInstance({
+      axios: mockAxios,
+      SOUNDSLICE_APPLICATION_ID,
+      SOUNDSLICE_PASSWORD,
+    });
+
+    const slug = `1357`;
+    const unusedResult = await apiClient.deleteSliceBySlug(slug);
+
+    expect(mockAxios.delete).toHaveBeenCalled();
+    expect(mockAxios.delete).toHaveBeenCalledWith(
+      `/scores/1357/`,
+      EXPECTED_BASE_AXIOS_CONFIG,
+    );
+  });
+});
